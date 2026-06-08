aa3a302442a72cb600640af310c48c4b946577c8
ifeson-jonas <ifeson68@gmail.com>
2026-06-07 22:47:15 -0300

    feat: add watch mode

--- Program.cs
+++ Program.cs
@@ -1,18 +1,50 @@
 ﻿using ServerMonitorCLI.Models;
 using ServerMonitorCLI.Services;
 using ServerMonitorCLI.Utils;
+using ServerMonitorCLI.Reports;
+
+var reportMode = args.Contains("--report");
+var watchMode = args.Contains("--watch");
 
 var cpuService = new CpuService();
 var memoryService = new MemoryService();
 var diskService = new DiskService();
 var uptimeService = new UptimeService();
 
-var serverInfo = new ServerInfo
+if (watchMode)
 {
-    CpuUsage = await cpuService.GetCpuUsageAsync(),
-    MemoryUsage = memoryService.GetMemoryUsagePercentage(),
-    DiskUsage = diskService.GetDiskUsage(),
-    Uptime = uptimeService.GetUptime()
-};
+    while (true)
+    {
+        var serverInfo = new ServerInfo
+        {
+            CpuUsage = await cpuService.GetCpuUsageAsync(),
+            MemoryUsage = memoryService.GetMemoryUsagePercentage(),
+            DiskUsage = diskService.GetDiskUsage(),
+            Uptime = uptimeService.GetUptime()
+        };
 
-ConsoleFormatter.PrintServerInfo(serverInfo);
+        Console.Clear();
+
+        ConsoleFormatter.PrintServerInfo(serverInfo);
+
+        await Task.Delay(2000);
+    }
+}
+else
+{
+    var serverInfo = new ServerInfo
+    {
+        CpuUsage = await cpuService.GetCpuUsageAsync(),
+        MemoryUsage = memoryService.GetMemoryUsagePercentage(),
+        DiskUsage = diskService.GetDiskUsage(),
+        Uptime = uptimeService.GetUptime()
+    };
+
+    ConsoleFormatter.PrintServerInfo(serverInfo);
+
+    if (reportMode)
+    {
+        var reportGenerator = new ReportGenerator();
+        reportGenerator.Generate(serverInfo);
+    }
+}
